9299b3b117a1347d9c57884030ceeebed36b7057
Bogdan Cuza <bogdancuza@protonmail.com>
2022-05-31 18:49:41 +0300

    commit tree 7dac40fa83c155ffd7ca010fb5a57f8aebd3ce62

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+    attr_reader :discount, :items
+    attr_accessor :total
 
+    def initialize(discount = 0)
+        @discount = discount
+        @items = []
+        @total = 0
+    end
+
+    def add_item(name, price, q = 1)
+        q.times {@items << name}
+        @total += price * q
+        @last_transaction = {:name => name, :price => price, :q => q}
+    end
+
+    def apply_discount
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            @total -= @discount * total/100
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def void_last_transaction
+        items.pop
+        @total -= @last_transaction[:price] * @last_transaction[:q]
+    end
+end
